e9b3306841d046df5f984ed288be2ec45a641352
nijunjie <nijunjie@corp.netease.com>
2021-10-11 00:14:17 +0800

    去除引入C模块,恢复tree-shaking能力

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { name } from "./moduleA";
 import { nameB } from "./moduleB";
-const moduleC = require('./moduleC')
+// const moduleC = require('./moduleC'); // 一旦esm和cjs混合使用，就会导致tree-shaking失效
 
-console.log(name, nameB, moduleC.nameC)
+console.log(name, nameB)
